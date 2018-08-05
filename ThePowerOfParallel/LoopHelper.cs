d9277691a44ae8fd75875fd9267cd1d055d0472c
Stefan Bolling <stefan.bolling@swedavia.se>
2018-08-05 17:35:54 +0200

    Added a console-app to demonstrate the power of parallel.

---
+++ ThePowerOfParallel/LoopHelper.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ThePowerOfParallel
+{
+    public static class LoopHelper
+    {
+        internal static double NormalForLoop(int numberOfIterations)
+        {
+            var startTime = DateTime.Now;
+            for (var i = 0; i < numberOfIterations; i++)
+            {
+                Program.SlowOperation();
+            }
+
+            var totalRunTime = DateTime.Now - startTime;
+
+            return totalRunTime.TotalSeconds;
+        }
+
+        internal static double ParallelForLoop(int numberOfIterations)
+        {
+            var startTime = DateTime.Now;
+
+            Parallel.For(0, numberOfIterations,
+                index => { Program.SlowOperation();
+                } );
+
+            var totalRunTime = DateTime.Now - startTime;
+
+            return totalRunTime.TotalSeconds;
+        }
+
+        internal static double NormalForEachLoop(int numberOfIterations)
+        {
+            var listOfObjects = new List<bool>();
+
+            for (var i = 0; i < numberOfIterations; i++)
+            {
+                listOfObjects.Add(true);
+            }
+
+            var startTime = DateTime.Now;
+
+            foreach (var listObject in listOfObjects)
+            {
+                Program.SlowOperation();
+            }
+
+            var totalRunTime = DateTime.Now - startTime;
+
+            return totalRunTime.TotalSeconds;
+        }
+
+        internal static double ParallelForEachLoop(int numberOfIterations)
+        {
+            // Notice that I don't use List in a paralell contect, since it is not thread safe.
+            var listOfObjects = new ConcurrentBag<bool>();
+
+            for (var i = 0; i < numberOfIterations; i++)
+            {
+                listOfObjects.Add(true);
+            }
+
+            var startTime = DateTime.Now;
+
+            Parallel.ForEach(listOfObjects, currentObject => 
+            {
+                Program.SlowOperation();
+            });
+
+            var totalRunTime = DateTime.Now - startTime;
+
+            return totalRunTime.TotalSeconds;
+        }
+    }
+}
---
+++ ThePowerOfParallel/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace ThePowerOfParallel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numberOfIterations = 10;
+            Console.WriteLine($"How much faster is parallel.for in {numberOfIterations} iterations");
+            Console.WriteLine($"Normal for-loop run in {LoopHelper.NormalForLoop(numberOfIterations)} seconds");
+            Console.WriteLine($"Parallel for-loop run in {LoopHelper.ParallelForLoop(numberOfIterations)} seconds");
+            Console.WriteLine($"Normal foreach-loop run in {LoopHelper.NormalForEachLoop(numberOfIterations)} seconds");
+            Console.WriteLine($"Parallel foreach-loop run in {LoopHelper.ParallelForEachLoop(numberOfIterations)} seconds");
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+
+        public static void SlowOperation()
+        {
+            Thread.Sleep(500);
+        }
+    }
+}
